4b6eeaba29bfd0faebe62b20ad53701244b76418
Michal Dovrtel <m.dovrtel@gmail.com>
2017-09-12 19:51:41 +0200

    CODE: Initial webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,18 @@ module.exports = {
     },
     devtool: 'source-map',
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     presets: ['react', 'es2015']
                 }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
