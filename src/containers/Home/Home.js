adb3af600442dcf87ce5ba06bf2d9cf208c4d2fa
Michal Dovrtel <m.dovrtel@gmail.com>
2017-09-25 18:43:06 +0200

    CODE: Initial react app

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+import theme from './Home.scss';
+
+export default class Home extends Component {
+
+	render () {
+
+		return (
+				<div className='title'>
+          Hello
+				</div>
+			);
+	}
+}
