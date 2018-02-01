d7a025c841f710a748716c579dedb14d379ca3a7
Michal Dovrtel jr <michal.dovrtel.jr@inzata.com>
2018-02-01 15:33:05 +0100

    CODE: Animate page transition

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { withRouter, Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import theme from './App.scss';
-import { CSSTransition } from 'react-transition-group';
-import TransitionGroup from 'react-transition-group/TransitionGroup';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+// import TransitionGroup from 'react-transition-group/TransitionGroup';
 import MobileDetect from 'mobile-detect';
 
 import Home from 'Home/Home';
@@ -10,7 +11,11 @@ import Test from 'Test/Test';
 import IaghIcon from 'Icons/Iagh';
 import Pong from 'Pong/Pong';
 
-export default class App extends Component {
+class App extends Component {
+	static propTypes = {
+		location: PropTypes.any,
+	};
+
 	constructor(props){
 		super(props);
 		this.state = {
@@ -90,6 +95,9 @@ export default class App extends Component {
 
 	render() {
 		const {pong, anim, animSpeech, iaghCount, iaghSpeech} = this.state;
+		const {location} = this.props;
+		const currentKey = location.pathname.split('/')[1] || '/';
+		const timeout = { enter: 475, exit: 375 };
 		return (
 			<div className={theme.main}>
 				{!anim && <div className={theme.pongBtn} onClick={this.pongHandle}></div>}
@@ -110,11 +118,34 @@ export default class App extends Component {
 				</CSSTransition>
 				{animSpeech && <div key='IaghSpeach' className={theme['oval-speech-border']}>{iaghSpeech}</div>}
 				{!pong &&
-				<Switch>
-					<Route exact path='/' component={Home}/>
-					<Route exact path='/test' component={Test}/>
-				</Switch>}
+					<TransitionGroup component="main" className={theme.pageRoute}>
+						<CSSTransition
+							appear
+							key={currentKey}
+							timeout={timeout}
+							classNames={{
+								appear: theme.in,
+								appearActive: theme.inActive,
+								enter: theme.in,
+								enterActive: theme.inActive,
+								exit: theme.out,
+								exitActive: theme.outActive
+							}}
+							mountOnEnter={true}
+							unmountOnExit={true}
+							>
+								<section className={theme.pageMainInner}>
+									<Switch location={location}>
+										<Route exact path='/' component={Home}/>
+										<Route exact path='/test' component={Test}/>
+									</Switch>
+								</section>
+							</CSSTransition>
+					</TransitionGroup>
+				}
 			</div>
 		);
 	}
 }
+
+export default withRouter(App);
