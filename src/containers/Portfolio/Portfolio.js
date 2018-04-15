3aa962bdd8d9f1431d8764548d598c65d69175c0
Michal Dovrtel <m.dovrtel@gmail.com>
2018-04-15 14:47:36 +0200

    CODE: Dialog for portfolio page

--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -5,7 +5,10 @@ import { withStyles } from 'material-ui/styles';
 import theme from './PortfolioTheme.js';
 import MasonryLayout from 'react-masonry-layout';
 import Zoom from 'material-ui/transitions/Zoom';
+import Slide from 'material-ui/transitions/Slide';
+import Dialog from 'material-ui/Dialog';
 import { Scrollbars } from 'react-custom-scrollbars';
+import PortfolioPage from './PortfolioPage';
 
 @connect(
 		state => ({
@@ -22,18 +25,28 @@ export default class Portfolio extends Component {
 		classes: PropTypes.any,
 	};
 
+	constructor(props){
+		super(props);
+		this.state = {
+			dialog: false,
+			project: '',
+		};
+	}
+
 	getMasonry = () => {
 		const {classes, portfolio} = this.props;
 		let masonry = [];
-		Object.keys(portfolio).map((k, i) => {
+		portfolio.map((obj, i) => {
 			// let height = i % 2 === 0 ? Math.floor(Math.random() * (200 + 1) + 300) : Math.floor(Math.random() * (200 + 1) + 100);
+			console.log('obj: ', obj, i);
 			let height = i % 2 === 0 ? 200 : 300;
-			let mainImg = k+'_1.png';
-			let title = portfolio[k].title;
+			let id = obj.id;
+			let mainImg = id+'_1.png';
+			let title = obj.title;
 			let image = '/store/images/'+mainImg;
 			masonry.push(
-				<Zoom key={k+masonry.length} in={true} style={{transitionDelay: i*20}}>
-					<div className={classes.masonryContainer} style={{height: `${height}px`}}>
+				<Zoom key={id+masonry.length} in={true} style={{transitionDelay: i*20}}>
+					<div className={classes.masonryContainer} style={{height: `${height}px`}} onClick={this.openDialog.bind(this, obj, i)}>
 						<div style={{height: '100%', width: '100%', background: 'url('+image+') center/cover'}}/>
 						<div className={classes.infoHover}>{title}</div>
 					</div>
@@ -43,26 +56,25 @@ export default class Portfolio extends Component {
 		return masonry;
 	}
 
+	transition = (props) => {
+		return <Slide direction="up" {...props} />;
+	}
+
+	openDialog = (obj, i) => {
+		this.setState({dialog: true, project: obj, index: i});
+	}
+
+	handleClose = () => {
+		this.setState({dialog: false, project: ''});
+	}
+
 	render() {
 		const {classes, portfolio} = this.props;
+		const {dialog, project} = this.state;
 console.log('portfolio: ', this.props.portfolio);
 		return (
 			<Scrollbars>
 				<div className={classes.pageWrapper}>
-			<div className={classes.pageWrapper}>
-				<MasonryLayout
-					id="masonry-layout"
-					infiniteScroll={this.loadItems}
-					sizes = {[
-						{ columns: 1, gutter: 10 },
-						{ mq: '768px', columns: 2, gutter: 15 },
-						{ mq: '1024px', columns: 3, gutter: 20 },
-						{ mq: '1440px', columns: 4, gutter: 20 },
-						{ mq: '1900px', columns: 5, gutter: 20 }
-					]}>
-					{this.getMasonry()}
-				</MasonryLayout>
-			</div>
 						<MasonryLayout
 							id="masonry-layout"
 							infiniteScroll={this.loadItems}
@@ -75,6 +87,14 @@ console.log('portfolio: ', this.props.portfolio);
 							]}>
 							{this.getMasonry()}
 						</MasonryLayout>
+						<Dialog
+							fullScreen
+							open={dialog}
+							onClose={this.handleClose}
+							transition={this.transition}
+							>
+								<PortfolioPage project={project} handleClose={this.handleClose}/>
+							</Dialog>
 				</div>
 			</Scrollbars>
 		);
