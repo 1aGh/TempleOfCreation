210dc8543069193b309b38ae7aef983bde5529f8
Michal Dovrtel <m.dovrtel@gmail.com>
2018-04-15 14:48:38 +0200

    CODE: Add portfolio items

--- store/portfolioStore.js
+++ store/portfolioStore.js
@@ -1,10 +1,25 @@
-const portfolioStore = {
-	motylekSurf: {
+const portfolioStore = [
+	{
+		id: 'motylekSurf',
 		title: 'Motýlek - SURF',
 		autor: 'Petr Mojžíšek',
 		year: '2017',
 		description: 'Jedná se o soutěžní návrh pro firmu BeWooden. Návrh SURF edice v sobě reflektuje tradiční metody výroby surfových prken. Klížení různých odrůd dřeva imituje jádro surfového prkna.',
-	}
-};
+	},
+	{
+		id: 'studentContainer',
+		title: 'Student Container',
+		autor: 'Václav Šipoš',
+		year: '2014',
+		description: 'V projektu šlo o návrh studentského bydlení v přepravním kontejneru. Návrh respektuje stanovený rozpočet projektu.',
+	},
+	{
+		id: 'kuchynskyKartac',
+		title: 'Kuchyňský kartáč',
+		autor: 'Petr Mojžíšek',
+		year: '201ř',
+		description: 'Paní Salvetová nás oslovila s prosbou o zpracování designu kuchyňského kartáče. Design měl být hravý, přičemž se měl podřizovat funkci. Po prvotních skicách a konzultacích jsme se dopracovali k finální podobě.',
+	},
+];
 
 export default portfolioStore;
