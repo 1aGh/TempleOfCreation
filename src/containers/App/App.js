f9bed8d5e289d9f9abdc81854bb6de08b0c1f613
Michal Dovrtel jr <michal.dovrtel.jr@inzata.com>
2018-04-23 18:42:54 +0200

    CODE: params for particles

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -21,6 +21,7 @@ import { create } from 'jss';
 import themeMui from 'theme/themeMui.js';
 import theme from './AppTheme.js';
 import Particles from 'react-particles-js';
+import particlesParams from './particlesParams.js';
 
 const generateClassName = createGenerateClassName({
 	productionPrefix: 'toc',
@@ -155,116 +156,7 @@ console.log('THEME: ', themeMui);
 									</CSSTransition>
 								</TransitionGroup>
 							}
-							<Particles className={classes.main} params={{
-							  "particles": {
-							    "number": {
-							      "value": 80,
-							      "density": {
-							        "enable": true,
-							        "value_area": 800
-							      }
-							    },
-							    "color": {
-							      "value": "#eab543"
-							    },
-							    "shape": {
-							      "type": "circle",
-							      "stroke": {
-							        "width": 0,
-							        "color": "#000000"
-							      },
-							      "polygon": {
-							        "nb_sides": 5
-							      },
-							      "image": {
-							        "src": "img/github.svg",
-							        "width": 100,
-							        "height": 100
-							      }
-							    },
-							    "opacity": {
-							      "value": 0.49705773886831206,
-							      "random": false,
-							      "anim": {
-							        "enable": false,
-							        "speed": 1,
-							        "opacity_min": 0.1,
-							        "sync": false
-							      }
-							    },
-							    "size": {
-							      "value": 3,
-							      "random": true,
-							      "anim": {
-							        "enable": false,
-							        "speed": 40,
-							        "size_min": 0.1,
-							        "sync": false
-							      }
-							    },
-							    "line_linked": {
-							      "enable": false,
-							      "distance": 150,
-							      "color": "#eab543",
-							      "opacity": 0.4,
-							      "width": 1
-							    },
-							    "move": {
-							      "enable": true,
-							      "speed": 0.5,
-							      "direction": "none",
-							      "random": true,
-							      "straight": false,
-							      "out_mode": "out",
-							      "bounce": false,
-							      "attract": {
-							        "enable": true,
-							        "rotateX": 1122.388442605866,
-							        "rotateY": 1122.388442605866
-							      }
-							    }
-							  },
-							  "interactivity": {
-							    "detect_on": "window",
-							    "events": {
-							      "onhover": {
-							        "enable": true,
-							        "mode": "grab"
-							      },
-							      "onclick": {
-							        "enable": true,
-							        "mode": "push"
-							      },
-							      "resize": true
-							    },
-							    "modes": {
-							      "grab": {
-							        "distance": 80,
-							        "line_linked": {
-							          "opacity": 1
-							        }
-							      },
-							      "bubble": {
-							        "distance": 400,
-							        "size": 40,
-							        "duration": 2,
-							        "opacity": 8,
-							        "speed": 3
-							      },
-							      "repulse": {
-							        "distance": 89.32849335314796,
-							        "duration": 0.4
-							      },
-							      "push": {
-							        "particles_nb": 4
-							      },
-							      "remove": {
-							        "particles_nb": 2
-							      }
-							    }
-							  },
-							  "retina_detect": true
-							}}/>
+							<Particles className={classes.main} params={particlesParams}/>
 						</div>
 					</MuiThemeProvider>
 			</JssProvider>
