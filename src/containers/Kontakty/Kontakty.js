15cf2270351d06581fb95a36c59fdc06793a928e
Michal Dovrtel <m.dovrtel@gmail.com>
2018-04-23 17:05:48 +0200

    CODE: Kontakty page to JSS

--- src/containers/Kontakty/Kontakty.js
+++ src/containers/Kontakty/Kontakty.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import theme from './Kontakty.scss';
-import Link from 'react-router-dom/Link';
+import PropTypes from 'prop-types';
+import theme from './KontaktyTheme.js';
+import { withStyles } from 'material-ui/styles';
+
+import Logo from 'Icons/Logo';
+
+@withStyles(theme)
 
 export default class Kontakty extends Component {
+	static propTypes = {
+		classes: PropTypes.any,
+	};
+
 	render() {
+		const {classes} = this.props;
 
 		return (
-			<div className={theme.testWrapper}>
-				Kontakty
-				<Link to="/">Show sidebar</Link>
+			<div className={classes.kontaktyWrapper}>
+				<div className={classes.row}>
+					<div className={classes.logo}><Logo/></div>
+				</div>
 			</div>
 		);
 	}
---
+++ src/containers/Kontakty/KontaktyTheme.js
@@ -0,0 +1,9 @@
+const theme = theme => ({
+	kontaktyWrapper: {
+		flex: '1 1 auto',
+		margin: '30px',
+		border: ['2px', 'solid', theme.colors.blackolive]
+	}
+});
+
+export default theme;
