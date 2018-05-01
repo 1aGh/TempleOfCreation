65ea2c227f8107d57174c57757d498ed8e0c5ae3
Michal Dovrtel <m.dovrtel@gmail.com>
2018-05-01 14:24:45 +0200

    CODE: Arrow icons

---
+++ src/components/Icons/Down.js
@@ -0,0 +1,10 @@
+/* eslint react/prop-types: 0 */
+import React from 'react';
+
+const Down = (props) => (
+  <svg {...props} viewBox="0 0 44 8" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
+    <path d="M22.186 6.485l-21.428-5.741-.349 1.301 21.774 5.834.003-.011.004.011 21.774-5.834-.349-1.301-21.429 5.741z" fill="#eab543"/>
+  </svg>
+);
+
+export default Down;
