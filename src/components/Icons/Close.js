a44c2a126b6b4580c90e7583cff71b4765f71a52
Michal Dovrtel <m.dovrtel@gmail.com>
2018-05-05 18:58:27 +0200

    CODE: close icon

---
+++ src/components/Icons/Close.js
@@ -0,0 +1,12 @@
+/* eslint react/prop-types: 0 */
+import React from 'react';
+
+const Close = (props) => (
+  <svg {...props} viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
+    <g strokeWidth="1.5" fill="none">
+      <path d="M1.544 42.622l41-41M1.544 1.622l41 41"/>
+    </g>
+  </svg>
+);
+
+export default Close;
