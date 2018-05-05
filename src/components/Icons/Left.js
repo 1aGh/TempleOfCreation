ea18b947ba034e0809d8a15706dd2fd191cdf059
Michal Dovrtel <m.dovrtel@gmail.com>
2018-05-05 11:00:36 +0200

    BUGFIX: default fill in arrows

--- src/components/Icons/Left.js
+++ src/components/Icons/Left.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Left = (props) => (
   <svg {...props} viewBox="0 0 8 44" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
-    <path d="M2.029 21.967l5.741-21.429-1.301-.348-5.834 21.774.011.003-.011.003 5.834 21.774 1.301-.349-5.741-21.428z" fill="#eab543"/>
+    <path d="M2.029 21.967l5.741-21.429-1.301-.348-5.834 21.774.011.003-.011.003 5.834 21.774 1.301-.349-5.741-21.428z"/>
   </svg>
 );
 
--- src/components/Icons/Up.js
+++ src/components/Icons/Up.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Up = (props) => (
   <svg {...props} viewBox="0 0 45 8" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
-    <path d="M22.539 1.448l21.428 5.742.349-1.302-21.774-5.834-.003.012-.003-.012-21.774 5.834.349 1.302 21.428-5.742z" fill="#eab543"/>
+    <path d="M22.539 1.448l21.428 5.742.349-1.302-21.774-5.834-.003.012-.003-.012-21.774 5.834.349 1.302 21.428-5.742z"/>
   </svg>
 );
 
