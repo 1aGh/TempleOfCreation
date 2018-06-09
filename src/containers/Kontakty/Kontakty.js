d54bc3d3f4956c46ef4d7d52d3fdfe4abc232f08
Michal Dovrtel <m.dovrtel@gmail.com>
2018-06-09 02:32:26 +0200

    CODE: Kontakty

--- src/containers/Kontakty/Kontakty.js
+++ src/containers/Kontakty/Kontakty.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import theme from './KontaktyTheme.js';
 import { withStyles } from '@material-ui/core/styles';
+import Envelope from 'Icons/Envelope';
+import Stamp from 'Icons/Stamp';
+import { Scrollbars } from 'react-custom-scrollbars';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Grid from '@material-ui/core/Grid';
 
 import Logo from 'Icons/Logo';
 
@@ -16,11 +23,88 @@ export default class Kontakty extends Component {
 		const {classes} = this.props;
 
 		return (
-			<div className={classes.kontaktyWrapper}>
-				<div className={classes.row}>
-					<div className={classes.logo}><Logo/></div>
+			<Scrollbars>
+				<div className={classes.dialogBar}>
+					<div className={classes.title}>Kontakty</div>
 				</div>
-			</div>
+				<div className={classes.kontaktyWrapper}>
+					<Typography variant='title' align='center' color='primary'>{'NAPIŠ NÁM'}</Typography>
+					<div className={classes.row}>
+						<div className={classes.form}>
+							<div className={classes.stamp}><Stamp/></div>
+							<Grid container spacing={24} direction='column' alignItems='flex-end'>
+								<Grid item container spacing={24} alignItems="center">
+									<Grid item xs='3'>
+										<Typography variant='caption' color='primary'>{'Kdo nám píše'}</Typography>
+									</Grid>
+									<Grid item>
+										<TextField
+											placeholder={'Jméno a příjmení'}
+											autoFocus
+											// value={this.state.multiline}
+											// onChange={this.handleChange('multiline')}
+											InputProps={{classes:{underline: classes.input, root: classes.input}}}
+										/>
+									</Grid>
+								</Grid>
+								<Grid item container spacing={24} alignItems="center">
+									<Grid item xs='3'>
+										<Typography variant='caption' color='primary'>{'Tvůj email'}</Typography>
+									</Grid>
+									<Grid item>
+										<TextField
+											placeholder={'@'}
+											type='email'
+											// value={this.state.multiline}
+											// onChange={this.handleChange('multiline')}
+											InputProps={{classes:{underline: classes.input, root: classes.input}}}
+										/>
+									</Grid>
+								</Grid>
+								<Grid item container spacing={24} alignItems="center">
+									<Grid item xs='3'>
+										<Typography variant='caption' color='primary'>{'Co potřebuješ'}</Typography>
+									</Grid>
+									<Grid item>
+										<TextField
+											placeholder={'Předmět emailu'}
+											// value={this.state.multiline}
+											// onChange={this.handleChange('multiline')}
+											InputProps={{classes:{underline: classes.input, root: classes.input}}}
+										/>
+									</Grid>
+								</Grid>
+							</Grid>
+						</div>
+						<div className={classes.contacts}>
+							<div className={classes.contactsContainer}>
+								<div className={classes.heading}>{'Fakturační údaje'}</div>
+								<p>{'Temple of Creation s.r.o.'}</p>
+								<p>{'Ostrá 2823/28'}</p>
+								<p>{'616 00 Brno, Žabovřesky'}</p>
+								<p>{'IČ: 064 37 311'}</p>
+								<p>{'DIČ: CZ06437311'}</p>
+							</div>
+						</div>
+					</div>
+					<div className={classes.messageContainer}>
+						<div className={classes.message}>
+							<Typography variant='caption' align='center' color='secondary' gutterBottom>{'Tvoje zpráva'}</Typography>
+							<TextField
+								id='multiline-flexible'
+								multiline
+								fullWidth
+								placeholder={'Ahoj'}
+								// value={this.state.multiline}
+								// onChange={this.handleChange('multiline')}
+								InputProps={{classes:{root: classes.textField}}}
+								margin='normal'
+							/>
+						</div>
+						<div className={classes.envelope}><Envelope/></div>
+					</div>
+				</div>
+			</Scrollbars>
 		);
 	}
 }
--- src/containers/Kontakty/KontaktyTheme.js
+++ src/containers/Kontakty/KontaktyTheme.js
@@ -1,8 +1,88 @@
 const theme = theme => ({
 	kontaktyWrapper: {
 		flex: '1 1 auto',
-		margin: '30px',
-		border: ['2px', 'solid', theme.colors.blackolive]
+		display: 'flex',
+		flexDirection: 'column',
+		margin: '5px',
+	},
+	row: {
+		flex: '0 0 280px',
+		display: 'flex',
+		flexDirection: 'row',
+		margin: '40px 100px',
+	},
+	dialogBar: {
+		width: '250px',
+		height: '50px',
+		background: theme.colors.sand,
+		position: 'fixed',
+		color: theme.colors.blackolive,
+		display: 'flex',
+		alignItems: 'center',
+		left: 0,
+		top: 0,
+		zIndex: 10,
+		'&:after': {
+			content: '""',
+			position: 'absolute',
+			bottom: '-50px',
+			left: '0',
+			width: '0',
+			height: '0',
+			borderRight: '250px solid transparent',
+			borderTop: ['50px', 'solid', theme.colors.sand],
+			zIndex: 10,
+		},
+	},
+	title: {
+		fontSize: '1.4rem',
+		textWrap: 'wrap',
+		paddingLeft: '20px',
+	},
+	form: {
+		flex: '1 1 auto',
+		display: 'flex',
+		justifyContent: 'flex-end',
+		flexDirection: 'column',
+	},
+	contacts: {
+		padding: '10px',
+		border: ['2px', 'dashed', theme.colors.sand],
+		color: theme.colors.sand,
+		textTransform: 'uppercase',
+		maxWidth: '300px',
+	},
+	heading: {
+		fontSize: '1.4rem',
+	},
+	message: {
+		backgroundColor: theme.colors.sand,
+		minHeight: '150px',
+		margin: '0 99px -20px 100px',
+		padding: '20px',
+	},
+	textField: {
+		fontSize: '1rem',
+	},
+	input: {
+		fontSize: '1.2rem',
+		padding: '0 5px',
+		backgroundColor: theme.colors.sand,
+		'&:after':{
+			display: 'none',
+		},
+		'&:before':{
+			display: 'none',
+		},
+	},
+	stamp: {
+		width: '100px',
+		height: '100px',
+		border: ['2px', 'dashed', theme.colors.sand],
+		marginBottom: '20px',
+		'&>svg': {
+			width: '110px',
+		}
 	}
 });
 
