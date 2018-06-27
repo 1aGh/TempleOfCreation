60e6dc9b6c0e584ad1234a837ea84ac55d54165b
Michal Dovrtel <m.dovrtel@gmail.com>
2018-06-27 22:23:58 +0200

    CODE: Get image from server

--- server/app.js
+++ server/app.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+var compression = require('compression')
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('dist'));
+app.use(compression({
+  filter: function (req, res) {
+    return true;
+  }
+}));
 
 app.get('/api/:id', (req, res) => {
   res.send(['Hello World!'+req.params.id]);
 });
 
-app.get('/api', (req, res) => {
-  res.send(['Hello World!']);
+app.get('/api/getFolder/:id', (req, res) => {
+  let folder = './store/images/'+req.params.id;
+  fs.readdir(folder, (err, files) => {
+    let filesAr = [];
+    if (files) {
+      files.map((file) => {
+        if (file.match(/^(.*\.(jpg|jpeg|png)$)?$/)) {
+          filesAr.push(file);
+        }
+      });
+      res.send(filesAr);
+    }
+  });
+});
+
+app.get('/api/store/images/:folder_id/:img_id', function (req, res, next) {
+  let fid = req.params.folder_id;
+  let iid = req.params.img_id;
+  let filepath = './store/images/'+fid+'/'+iid;
+  res.sendFile(filepath, { root : './' });
 });
 
 app.listen(PORT, () => {
